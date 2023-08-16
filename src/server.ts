ca74479016c081ede5958f6f8dc375b6c14f5026
beepek <ben.schylling@gmail.com>
2023-08-16 16:13:50 -0400

    please

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000; // Use the provided port or default to 3000
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.send('Hello World');
 });
 
 app.get('/menu', (req, res) => {
@@ -21,6 +21,6 @@ app.get('/contact', (req, res) => {
   res.render('contact');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
